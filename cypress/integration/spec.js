f0bf27f7c0a7cdaf328374a11a3a95f8456b536f
Matthieu Bergel <hello@matthieubergel.org>
2020-02-10 17:50:20 +0100

    test: add failing test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,11 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+  // it('works', () => {
+  //   cy.visit('https://example.cypress.io')
+  // })
+  it('fails', () => {
+    cy.visit('https://ourworldindata.org/polio')
+    // cy.wait(5000) // no effect
+    cy.contains('here').click() // error: CypressError: Timed out retrying: Constructor is not a constructor
   })
 })
